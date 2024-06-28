8af52c225985e302c5982dd8f724a2a011499e1a
Avantika-786 <avantikaharale81@gmail.com>
2024-06-28 23:54:05 +0530

    cors updated

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const cookieParser = require("cookie-parser");
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
 );
