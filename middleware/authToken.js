68a38843711f15f69bec1f0eb7edcd2263c01415
Avantika-786 <avantikaharale81@gmail.com>
2024-06-30 16:30:36 +0530

    envchange

--- middleware/authToken.js
+++ middleware/authToken.js
@@ -5,7 +5,7 @@ exports.authToken = (req, res, next) => {
   try {
     const token =
       req.cookies?.token || req.headers.authorization?.split(" ")[1];
-
+    console.log(token);
     if (!token) {
       return res.status(401).json({
         message: "Please Log In",
